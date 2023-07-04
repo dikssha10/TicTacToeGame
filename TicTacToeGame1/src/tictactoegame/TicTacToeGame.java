d947a99005bcd74341fd201741805abf29775c29
Diksha Mendhe <diksshamenddhe97@gmail.com>
2023-07-04 18:54:01 +0530

    first commit

---
+++ TicTacToeGame1/src/tictactoegame/TicTacToeGame.java
@@ -0,0 +1,16 @@
+package tictactoegame;
+
+public class TicTacToeGame {
+    public static void main(String[] args) {
+        int n = 10;
+        char[] board = new char[n];
+
+        for (int i = 0; i < n; i++) ;
+        {
+            for (int j = 0; j < n; j++) ;
+            {
+                board[n] = '-';
+            }
+        }
+    }
+}
